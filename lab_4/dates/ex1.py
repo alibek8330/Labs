8487477dfeb543d996713293a0259001bb09e94a
alibek8330 <156774375+alibek8330@users.noreply.github.com>
2024-01-30 15:42:12 +0600

    update

---
+++ lab_4/dates/ex1.py
@@ -0,0 +1,14 @@
+# #DAY.MONTH.YEAR
+
+# date = input()
+
+# def convert_to_date(date):
+#     day, month, year = date.split('.')
+#     day = int(day)
+#     month = int(month)
+#     year = int(year)
+#     if day < 5:
+        
+#     return day, month, year
+
+# print(convert_to_date(date))
