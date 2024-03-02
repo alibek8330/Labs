7682ca98357648f169f2010c22ecc17fd8fbd03e
alibek8330 <156774375+alibek8330@users.noreply.github.com>
2024-03-03 01:28:42 +0600

    Add files via upload

--- lab_6/builtin functions/ex4.py
+++ lab_6/builtin functions/ex4.py
@@ -1,2 +1,11 @@
-import math
 from time import sleep
+import math
+
+def sqrtfunc(sqrtfunc, ms, *args):
+  sleep(ms / 1000)
+  return sqrtfunc(*args)
+
+n = int(input("Enter a number: "))
+ms = int(input("Enter a number of milliseconds: "))
+print("Square root after specific milliseconds:")
+print(sqrtfunc(lambda x: math.sqrt(x), ms, n))
